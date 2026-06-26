7deb8834f2c5631b16e70bbab86eacfd8be66312
houliang2001 <70315450+houliang2001@users.noreply.github.com>
2026-06-26 14:59:42 +0800

    Upload src App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="app-shell">
+    <AppHeader />
+    <main>
+      <RouterView v-slot="{ Component }">
+        <Transition name="page-fade" mode="out-in">
+          <component :is="Component" />
+        </Transition>
+      </RouterView>
+    </main>
+  </div>
+</template>
+
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+</script>
